eae3bdf0eb482ea9fc75953647c2ec542e117aa6
gawasa29 <yusei112929@gmail.com>
2025-11-16 16:20:04 +0900

    feat: add button component, search page, and update styles and dependencies

---
+++ app/search/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <main className="flex min-h-screen items-center justify-center">
+        <h1>Search Page</h1>
+      </main>
+    </div>
+  )
+}
